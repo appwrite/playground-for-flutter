951e06440297beffaba9563098db259d88a684cb
Eldad Fux <eldad.fux@gmail.com>
2020-04-11 07:17:30 +0300

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,175 @@
 import 'package:flutter/material.dart';
+import 'package:appwrite/appwrite.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  Client client = Client();
+  Account account = Account(client);
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  client
+      .setEndpoint('https://localhost/v1') // Your project ID
+      .setProject('5e8cf4f46b5e8') // Your project ID
+      .setSelfSigned()
+  ;
+  
+  runApp(
+    new MaterialApp(
+      home: new Playground(account: account),
+    )
+  );
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Playground extends StatefulWidget {
+  Playground({this.account});
+  final Account account;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  PlaygroundState createState() => new PlaygroundState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class PlaygroundState extends State<Playground> {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String username = "Loading...";
+
+  @override
+  void initState() {
+    widget.account.get()
+      .then((response) {
+        setState(() {
+          username = response.data['name'];
+        });
+      })
+      .catchError((error) {
+        setState(() {
+          username = 'Anonymous User';
+        });
+      });
+
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Appwrite + Flutter = ❤️"), backgroundColor: Colors.pinkAccent[200]),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Padding(padding: new EdgeInsets.all(20.0)),
+
+              ButtonTheme(
+                minWidth: 280.0,
+                height: 50.0,
+                child: new RaisedButton(
+                  child: new Text("Login with Facebook", style: new TextStyle(color: Colors.white, fontSize: 20.0)),
+                  color: Colors.blue,
+                  onPressed: () {
+                    widget.account.createOAuth2Session(provider: 'facebook', success: '', failure: '')
+                      .then((value) {
+                          widget.account.get()
+                          .then((response) {
+                            setState(() {
+                              username = response.data['name'];
+                            });
+                          })
+                          .catchError((error) {
+                            setState(() {
+                              username = 'Anonymous User';
+                            });
+                          });
+                      });
+                  }
+                ),
+              ),
+
+              new Padding(padding: new EdgeInsets.all(10.0)),
+
+              ButtonTheme(
+                minWidth: 280.0,
+                height: 50.0,
+                child: new RaisedButton(
+                  child: new Text("Login with GitHub", style: new TextStyle(color: Colors.white, fontSize: 20.0)),
+                  color: Colors.black87,
+                  onPressed: () {
+                    widget.account.createOAuth2Session(provider: 'github', success: '', failure: '')
+                      .then((value) {
+                        widget.account.get()
+                        .then((response) {
+                          setState(() {
+                            username = response.data['name'];
+                          });
+                        })
+                        .catchError((error) {
+                          setState(() {
+                            username = 'Anonymous User';
+                          });
+                        });
+                      });
+                  }
+                ),
+              ),
+
+              new Padding(padding: new EdgeInsets.all(10.0)),
+
+              ButtonTheme(
+                minWidth: 280.0,
+                height: 50.0,
+                child: new RaisedButton(
+                  child: new Text("Login with Google", style: new TextStyle(color: Colors.white, fontSize: 20.0)),
+                  color: Colors.red,
+                  onPressed: () {
+                    widget.account.createOAuth2Session(provider: 'google', success: '', failure: '')
+                      .then((value) {
+                        widget.account.get()
+                        .then((response) {
+                          setState(() {
+                            username = response.data['name'];
+                          });
+                        })
+                        .catchError((error) {
+                          setState(() {
+                            username = 'Anonymous User';
+                          });
+                        });
+                      });
+                  }
+                ),
+              ),
+              
+              new Padding(padding: new EdgeInsets.all(20.0)),
+              new Divider(),
+              new Padding(padding: new EdgeInsets.all(20.0)),
+              
+              new Text(username, style: new TextStyle(color: Colors.black, fontSize: 20.0)),
+              
+              new Padding(padding: new EdgeInsets.all(20.0)),
+              new Divider(),
+              new Padding(padding: new EdgeInsets.all(20.0)),
+
+              ButtonTheme(
+                minWidth: 280.0,
+                height: 50.0,
+                child: new RaisedButton(
+                  child: new Text('Logout', style: new TextStyle(color: Colors.white, fontSize: 20.0)),
+                  color: Colors.red[700],
+                  onPressed: () {
+                    widget.account.deleteSession(sessionId: 'current')
+                      .then((response) {
+                        setState(() {
+                          username = 'Anonymous User';
+                        });
+                      }).catchError((error) {
+                        print('error');
+                        print(error.response.data);
+                      });
+                  }
+                ),
+              ),
+            ]
+          )
+        )
+      )
     );
   }
 }
